ddd87496f6aeef578a98711bb242502e18343661
Theophilus Firtandi <theophilus.josiah@hotmail.com>
2023-06-07 16:08:54 -0400

    Change bg color, add padding

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-// import WarehousesPage from "../src/pages/WarehousesPage/WharehousesPage";
-// import WarehouseAddPage from "../src/pages/WarehouseAddPage/WharehouseAddPage";
-// import WarehouseEditPage from "../src/pages/WarehouseEditPage/WharehouseEditPage";
-// import WarehouseDetailsPage from "../src/pages/WarehouseDetailsPage/WharehouseDetailsPage";
+import WarehousesPage from "./pages/WarehousesPage/WarehousePage";
+// import WarehouseAddPage from "../src/pages/WarehouseAddPage/WarehouseAddPage";
+// import WarehouseEditPage from "../src/pages/WarehouseEditPage/WarehouseEditPage";
+// import WarehouseDetailsPage from "../src/pages/WarehouseDetailsPage/WarehouseDetailsPage";
 // import InventoriesPage from "../src/pages/InventoriesPage/InventoriesPage";
 // import InventoriesAddPage from "../src/pages/InventoriesAddPage/InventoriesAddPage";
 // import InventoriesEditPage from "../src/pages/InventoriesEditPage/InventoriesEditPage";
@@ -15,17 +15,17 @@ function App() {
   return (
     <Router>
       <Header />
-      {/*<Routes>
-        <Route path="/" element={<WarehousesPage/>}/>
-        <Route path="/warehouses/" element={<WarehousesPage/>}/>
-        <Route path="/warehouses/add" element={<WarehouseAddPage/>}/>
-        <Route path="/warehouses/:id" element={<WarehouseDetailsPage/>} />
-        <Route path="/warehouses/:id/edit" element={<WarehouseEditPage/>} />
-        <Route path="/inventory" element={<InventoryPage/>}/>
-        <Route path="/inventory/add" element={<InventoryAddPage/>}/>
-        <Route path="/inventory/:id" element={<InventoryDetailsPage/>}/>
-        <Route path="/inventory/:id/edit" element={<InventoryEditPage/>}/>
-      </Routes>*/}
+      <Routes>
+        <Route path="/" element={<WarehousesPage />} />
+        <Route path="/warehouses/" element={<WarehousesPage />} />
+        {/* <Route path="/warehouses/add" element={<WarehouseAddPage />} />
+        <Route path="/warehouses/:id" element={<WarehouseDetailsPage />} />
+        <Route path="/warehouses/:id/edit" element={<WarehouseEditPage />} />
+        <Route path="/inventory" element={<InventoryPage />} />
+        <Route path="/inventory/add" element={<InventoryAddPage />} />
+        <Route path="/inventory/:id" element={<InventoryDetailsPage />} />
+        <Route path="/inventory/:id/edit" element={<InventoryEditPage />} /> */}
+      </Routes>
       <Footer />
     </Router>
   );
