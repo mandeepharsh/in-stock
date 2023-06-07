d467be9d84268d480077500f68cede8eaea2d68a
Tina Thakur <tina.thakur.acc@gmail.com>
2023-06-07 11:20:03 -0400

    create folder pages, create subfolder for each Page, within each subfolder create the .jsx file with the empty function inside

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Footer from "./components/Footer/Footer";
 // import WarehouseAddPage from "../src/pages/WarehouseAddPage/WharehouseAddPage";
 // import WarehouseEditPage from "../src/pages/WarehouseEditPage/WharehouseEditPage";
 // import WarehouseDetailsPage from "../src/pages/WarehouseDetailsPage/WharehouseDetailsPage";
-// import InventoryPage from "../src/pages/InventoryPage/InventoryPage";
-// import InventoryAddPage from "../src/pages/InventoryAddPage/InventoryAddPage";
-// import InventoryEditPage from "../src/pages/InventoryEditPage/InventoryEditPage";
-// import InventoryDetailsPage from "../src/pages/InventoryDetailsPage/InventoryDetailsPage";
+// import InventoriesPage from "../src/pages/InventoriesPage/InventoriesPage";
+// import InventoriesAddPage from "../src/pages/InventoriesAddPage/InventoriesAddPage";
+// import InventoriesEditPage from "../src/pages/InventoriesEditPage/InventoriesEditPage";
+// import InventoriesDetailsPage from "../src/pages/InventoriesDetailsPage/InventoriesDetailsPage";
 
 function App() {
   return (
@@ -21,10 +21,10 @@ function App() {
         <Route path="/warehouses/add" element={<WarehouseAddPage/>}/>
         <Route path="/warehouses/:id" element={<WarehouseDetailsPage/>} />
         <Route path="/warehouses/:id/edit" element={<WarehouseEditPage/>} />
-        <Route path="/inventory" element={<InventoryPage/>}/>
-        <Route path="/inventory/add" element={<InventoryAddPage/>}/>
-        <Route path="/inventory/:id" element={<InventoryDetailsPage/>}/>
-        <Route path="/inventory/:id/edit" element={<InventoryEditPage/>}/>
+        <Route path="/inventories" element={<InventoriesPage/>}/>
+        <Route path="/inventories/add" element={<InventoriesAddPage/>}/>
+        <Route path="/inventories/:id" element={<InventoriesDetailsPage/>}/>
+        <Route path="/inventories/:id/edit" element={<InventoriesEditPage/>}/>
       </Routes>*/}
       <Footer />
     </Router>
