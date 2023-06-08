b5ee565c04c85a16a1f05e1f9ede0059cec16efd
mandeepharsh <harshman47@gmail.com>
2023-06-08 00:45:10 -0400

    add form validation, add link to the main page, fix margin bottom on the warehouseadd page

--- src/pages/WarehouseAddPage/WarehouseAddPage.js
+++ src/pages/WarehouseAddPage/WarehouseAddPage.js
@@ -4,10 +4,15 @@ import axios from "axios";
 
 // assets
 import arrowback from "../../assets/icons/arrow_back-24px.svg";
-import { URL } from "../../utils/api";
+import errorIcon from "../../assets/icons/error-24px.svg";
+import {Link} from "react-router-dom";
 
 // styling
 import "./WarehouseAddPage.scss";
+
+// api
+import { URL } from "../../utils/api";
+
 
 const WarehouseAddpage = () => {
 const initialValues = {
@@ -21,7 +26,19 @@ const initialValues = {
     contact_email: ""      
 }
 
+const initialErrorState = {
+  warehouse_name: false,
+  address: false,
+  city: false,
+  country: false,
+  contact_name: false,
+  contact_position: false,
+  contact_phone: false,
+  contact_email: false,  
+}
+
 const [values,setValues] = useState(initialValues);
+const [errors, setErrors] = useState(initialErrorState);
 
 const onChangeHandler = (event) =>{
     const {name,value} = event.target;
@@ -29,103 +46,144 @@ const onChangeHandler = (event) =>{
         ...values,
         [name]: value
     });
+
+    setErrors({
+      ...errors,
+      [name]: value
+    })
 };
 
 
 
-const addWarehouseHandler = () =>{
+const addWarehouseHandler = (event) =>{
+  event.preventDefault();
+  
+  const newErrors = {};
+  let hasError = false;
+  for (let field in values) {
+    if (values[field] === "") {
+      newErrors[field] = true;
+      hasError = true;
+    } else {
+      newErrors[field] = false;
+    }
+  }
+
+  if (hasError) {
+    setErrors(newErrors);
+  } else {
     axios.post(URL + "/add",values)
-    .then((response) => console.log(response))
-    .catch((error) =>{
-      console.log(error)
-    })
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  }
 }
   return (
     <div className="warehouse-add-page">
       <div className="warehouse-add-page__title">
-        <img className="warehouse-add-page__icon" src={arrowback} alt="arrow back icon" />
+       <Link to="/"> <img className="warehouse-add-page__icon"
+         src={arrowback} alt="arrow back icon" /></Link>
         <h1 className="warehouse-add-page__heading">Add New Warehouse</h1>
       </div>
       <hr className="warehouse-add__divider"/>
       <form className="warehouse-add__form"
             onSubmit={addWarehouseHandler} >
-        <div className="warehouse-add__fieldset">
+        <div className="warehouse-add__fieldset warehouse-add__fieldset--divider">
           <h2 className="warehouse-add__fieldset-heading">Warehouse Details</h2>
           <label className="warehouse-add__label">
             Warehouse Name
-            <input className="warehouse-add__input" 
+            <input className={`warehouse-add__input ${errors.warehouse_name ? "warehouse-add__input--invalid" : ""}`} 
                    name = "warehouse_name"
                    placeholder="Warehouse Name"
                    value ={values.warehouse_name }
                    onChange={onChangeHandler}
                     />
+           {errors.warehouse_name && <span className="warehouse-add__error-message">
+           <img alt="error icon" src={errorIcon}/>This field is required</span>}
           </label>
           <label className="warehouse-add__label">
             Street Address
-            <input className="warehouse-add__input" 
+            <input className={`warehouse-add__input ${errors.address ? "warehouse-add__input--invalid" : ""}`}
                    name = "address"
                    placeholder="Street Address" 
                    value = {values.address}
                    onChange={onChangeHandler}
-                   />
+                   />  
+           {errors.address && <span className="warehouse-add__error-message">
+           <img alt="error icon" src={errorIcon}/>This field is required</span>} 
           </label>
           <label className="warehouse-add__label">
             City
-            <input className="warehouse-add__input"
+            <input className={`warehouse-add__input ${errors.city ? "warehouse-add__input--invalid" : ""}`}
                    name="city"
                    placeholder="City"
                    value={values.city}
                    onChange={onChangeHandler}
                     />
+          {errors.city && <span className="warehouse-add__error-message">
+          <img alt="error icon" src={errorIcon}/>This field is required</span>}   
           </label>
           <label className="warehouse-add__label">
             Country
-            <input className="warehouse-add__input" 
+            <input className={`warehouse-add__input ${errors.country ? "warehouse-add__input--invalid" : ""}`} 
                    name="country"
                    placeholder="Country"
                    value={values.country}
                    onChange={onChangeHandler}
-                    />
+                    />     
+           {errors.country &&
+           <span className="warehouse-add__error-message">
+           <img alt="error icon" src={errorIcon}/>This field is required</span>}
           </label>
         </div>
-        <hr className="warehouse-add__divider"/>
+        <hr className="warehouse-add__divider warehouse-add__divider--remove"/>
         <div className="warehouse-add__fieldset">
           <h2 className="warehouse-add__fieldset-heading">Contact Details</h2>
           <label className="warehouse-add__label" >
             Contact Name
-            <input className="warehouse-add__input"
+            <input className={`warehouse-add__input ${errors.contact_name ? "warehouse-add__input--invalid" : ""}`}
                    name="contact_name" 
                    placeholder="Contact Name" 
                    value={values.contact_name}
                    onChange={onChangeHandler}
                    />
+        {errors.contact_name && <span className="warehouse-add__error-message">
+        <img alt="error icon" src={errorIcon}/>This field is required</span>}
           </label>
           <label className="warehouse-add__label">
             Position
-            <input className="warehouse-add__input" 
+            <input className={`warehouse-add__input ${errors.contact_name ? "warehouse-add__input--invalid" : ""}`} 
                    name="contact_position"
                    placeholder="Position" 
                    value={values.contact_position}
                    onChange={onChangeHandler} 
-                   />
+                   /> 
+           {errors.contact_position && <span className="warehouse-add__error-message">
+           <img alt="error icon" src={errorIcon}/>This field is required</span>}
+         
           </label>
           <label className="warehouse-add__label">
             Phone Number
-            <input className="warehouse-add__input" 
+            <input className={`warehouse-add__input ${errors.contact_phone ? "warehouse-add__input--invalid" : ""}`} 
                    name="contact_phone"
                    placeholder="Phone Number"
                    value={values.contact_phone}
                    onChange={onChangeHandler}
                     />
+           {errors.contact_phone && <span className="warehouse-add__error-message">
+           <img alt="error icon" src={errorIcon}/>This field is required</span>}
+        
           </label>
           <label className="warehouse-add__label">
             Email
-            <input className="warehouse-add__input" 
+            <input className={`warehouse-add__input ${errors.contact_email ? "warehouse-add__input--invalid" : ""}`} 
                    name="contact_email"
                    placeholder="Email" 
                    value={values.contact_email}
                    onChange={onChangeHandler} 
                    />
+          {errors.contact_email && 
+          <span className="warehouse-add__error-message">
+          <img alt="error icon" src={errorIcon}/>This field is required</span>} 
           </label>
         </div>
 
