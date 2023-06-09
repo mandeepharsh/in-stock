f253c76e84136b9eb14f234af6bdb4b91f1859a5
Tina Thakur <tinathakur@Tinas-MBP.localdomain>
2023-06-09 11:20:15 -0400

    pass down prop to toggle modal

--- src/components/DeleteModal/DeleteModal.jsx
+++ src/components/DeleteModal/DeleteModal.jsx
@@ -1,46 +1,46 @@
 import closeIcon from "../../assets/icons/close-24px.svg";
 import "./DeleteModal.scss";
-// import { useState } from "react";
+import React, { useState } from "react";
 
+export default function DeleteModal({ warehouse, toggleModal }) {
+  const [modalVisible, setModalVisible] = useState(false);
 
-export default function DeleteModal({warehouse}) {
-    // const [modal, setModal] = useState(false);
-  
-    // const toggleModal = () => {
-    //   setModal(!modal);
-    // };
-
-    // if(modal) {
-    //     document.body.classlist.add("active-modal")
-    // } else {
-    //     document.body.classlist.remove("active-modal")
-    // }
+  const closeModal = () => {
+    console.log("close modal");
+    setModalVisible(!modalVisible);
+  };
 
   return (
     <>
-        <div className="warehouse__modal">
-          <div className="warehouse__modal-overlay"></div>
-          <div className="warehouse__modal-content">
-            <a className="warehouse__modal-actions--x">
-              <img
-                src={closeIcon}
-                alt="x-close-icon"
-                className="warehouse__modal-icon"
-              />
-            </a>
-            <h1 className="warehouse__modal-header">Delete {warehouse.warehouse_name} warehouse?</h1>
-            <p className="warehouse__modal-message">
-              Please confirm that you’d like to delete the {warehouse.warehouse_name}  from the list of warehouses. You won’t be able to
-              undo this action.
-            </p>
-            <div className="warehouse__modal-actions">
-              <button className="warehouse__modal-actions--close">
-                Cancel
-              </button>
-              <button className="warehouse__modal-actions--delete">Delete</button>
-            </div>
+      <div className="warehouse__modal">
+        <div className="warehouse__modal-overlay"></div>
+        <div className="warehouse__modal-content">
+          <a className="warehouse__modal-actions--x" onClick={toggleModal}>
+            <img
+              src={closeIcon}
+              alt="x-close-icon"
+              className="warehouse__modal-icon"
+            />
+          </a>
+          <h1 className="warehouse__modal-header">
+            Delete {warehouse.warehouse_name} warehouse?
+          </h1>
+          <p className="warehouse__modal-message">
+            Please confirm that you’d like to delete the{" "}
+            {warehouse.warehouse_name} from the list of warehouses. You won’t be
+            able to undo this action.
+          </p>
+          <div className="warehouse__modal-actions">
+            <button
+              className="warehouse__modal-actions--close"
+              onClick={toggleModal}
+            >
+              Cancel
+            </button>
+            <button className="warehouse__modal-actions--delete">Delete</button>
           </div>
         </div>
+      </div>
     </>
   );
 }
--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -16,70 +16,72 @@ export default function WarehouseList({ warehouses }) {
 
   return (
     <>
-       {modal && (<DeleteModal warehouse={warehouse}/>)}
+      {modal && <DeleteModal warehouse={warehouse} toggleModal={toggleModal} />}
 
- 
-    <ul className="warehouses__list">
-      {warehouses.map((warehouse) => {
-        return (
-          <div key={warehouse.id} className="warehouses__details">
-            <div className="warehouses__warehouse-container">
-              <h4 className="warehouses__details-title">WAREHOUSE</h4>
-              <Link
-                to={`/warehouses/${warehouse.id}`}
-                className="warehouses__link"
-              >
-                <p className="warehouses__warehouse">
-                  {warehouse.warehouse_name}
-                  <img src={rightIcon} alt="chevron-pointing-right" />
+      <ul className="warehouses__list">
+        {warehouses.map((warehouse) => {
+          return (
+            <div key={warehouse.id} className="warehouses__details">
+              <div className="warehouses__warehouse-container">
+                <h4 className="warehouses__details-title">WAREHOUSE</h4>
+                <Link
+                  to={`/warehouses/${warehouse.id}`}
+                  className="warehouses__link"
+                >
+                  <p className="warehouses__warehouse">
+                    {warehouse.warehouse_name}
+                    <img src={rightIcon} alt="chevron-pointing-right" />
+                  </p>
+                </Link>
+              </div>
+
+              <div className="warehouses__name-container">
+                <h4 className="warehouses__details-title">CONTACT NAME</h4>
+                <p className="warehouses__name">{warehouse.contact_name}</p>
+              </div>
+
+              <div className="warehouses__address-container">
+                <h4 className="warehouses__details-title">ADDRESS</h4>
+                <p className="warehouses__address">
+                  {warehouse.address}, {warehouse.city}, {warehouse.country}
                 </p>
-              </Link>
-            </div>
+              </div>
 
-            <div className="warehouses__name-container">
-              <h4 className="warehouses__details-title">CONTACT NAME</h4>
-              <p className="warehouses__name">{warehouse.contact_name}</p>
-            </div>
+              <div className="warehouses__information-container">
+                <h4 className="warehouses__details-title">
+                  CONTACT INFORMATION
+                </h4>
+                <p className="warehouses__information-number">
+                  {warehouse.contact_phone}
+                </p>
+                <p className="warehouses__information-email">
+                  {warehouse.contact_email}
+                </p>
+              </div>
 
-            <div className="warehouses__address-container">
-              <h4 className="warehouses__details-title">ADDRESS</h4>
-              <p className="warehouses__address">
-                {warehouse.address}, {warehouse.city}, {warehouse.country}
-              </p>
-            </div>
-
-            <div className="warehouses__information-container">
-              <h4 className="warehouses__details-title">CONTACT INFORMATION</h4>
-              <p className="warehouses__information-number">
-                {warehouse.contact_phone}
-              </p>
-              <p className="warehouses__information-email">
-                {warehouse.contact_email}
-              </p>
-            </div>
-
-            <div className="warehouses__icons">
-              <img onClick={()=>toggleModal(warehouse)}
-                src={deleteIcon}
-                alt="garbage-delete-icon"
-                className="warehouses__icons-del"
-              />
-
-              <Link
-                to={`/warehouses/${warehouse.id}/edit`}
-                className="warehouses__icons-link"
-              >
+              <div className="warehouses__icons">
                 <img
-                  src={editIcon}
-                  alt="pencil-edit-icon"
-                  className="warehouses__icons-edit"
+                  onClick={() => toggleModal(warehouse)}
+                  src={deleteIcon}
+                  alt="garbage-delete-icon"
+                  className="warehouses__icons-del"
                 />
-              </Link>
+
+                <Link
+                  to={`/warehouses/${warehouse.id}/edit`}
+                  className="warehouses__icons-link"
+                >
+                  <img
+                    src={editIcon}
+                    alt="pencil-edit-icon"
+                    className="warehouses__icons-edit"
+                  />
+                </Link>
+              </div>
             </div>
-          </div>
-        );
-      })}
-    </ul>
+          );
+        })}
+      </ul>
     </>
   );
 }
