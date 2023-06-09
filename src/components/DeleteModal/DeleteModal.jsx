9a5298f23cfce4b8d1a26a17727cdca0f6b71e9b
Tina Thakur <tinathakur@Tinas-MBP.localdomain>
2023-06-09 09:55:55 -0400

    add props to deletemodal

--- src/components/DeleteModal/DeleteModal.jsx
+++ src/components/DeleteModal/DeleteModal.jsx
@@ -1,8 +1,20 @@
 import closeIcon from "../../assets/icons/close-24px.svg";
 import "./DeleteModal.scss";
+// import { useState } from "react";
 
 
 export default function DeleteModal({warehouse}) {
+    // const [modal, setModal] = useState(false);
+  
+    // const toggleModal = () => {
+    //   setModal(!modal);
+    // };
+
+    // if(modal) {
+    //     document.body.classlist.add("active-modal")
+    // } else {
+    //     document.body.classlist.remove("active-modal")
+    // }
 
   return (
     <>
@@ -16,9 +28,9 @@ export default function DeleteModal({warehouse}) {
                 className="warehouse__modal-icon"
               />
             </a>
-            <h1 className="warehouse__modal-header">Delete Manhattan warehouse?</h1>
+            <h1 className="warehouse__modal-header">Delete {warehouse.warehouse_name} warehouse?</h1>
             <p className="warehouse__modal-message">
-              Please confirm that you’d like to delete the Washington from the list of warehouses. You won’t be able to
+              Please confirm that you’d like to delete the {warehouse.warehouse_name}  from the list of warehouses. You won’t be able to
               undo this action.
             </p>
             <div className="warehouse__modal-actions">
