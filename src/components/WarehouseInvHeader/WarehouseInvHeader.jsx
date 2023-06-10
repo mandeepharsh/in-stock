c5514a716d08738a5fb28416cc17c9520a3f0bb2
Jeff Hamill <jthamill@gmail.com>
2023-06-10 15:44:22 -0400

    sor headings for mobile

--- src/components/WarehouseInvHeader/WarehouseInvHeader.jsx
+++ src/components/WarehouseInvHeader/WarehouseInvHeader.jsx
@@ -6,29 +6,29 @@ export default function WarehouseInvHeader() {
   return (
     <>
 
-      <div className="inventories__table-fields">
-        <div className="inventories__table-field">
-          <h4 className="inventories__table-header">INVENTORY ITEM</h4>
+      <div className="inventory__table-fields">
+        <div className="inventory__table-field">
+          <h4 className="inventory__table-header">INVENTORY ITEM</h4>
           <img src={sortIcon} alt="sort-icon-up-and-down-arrow" />
         </div>
 
-        <div className="inventories__table-field">
-          <h4 className="inventories__table-header">CATEGORY</h4>
+        <div className="inventory__table-field">
+          <h4 className="inventory__table-header">CATEGORY</h4>
           <img src={sortIcon} alt="sort-icon-up-and-down-arrow" />
         </div>
 
-        <div className="inventories__table-field">
-          <h4 className="inventories__table-header">STATUS</h4>
+        <div className="inventory__table-field">
+          <h4 className="inventory__table-header">STATUS</h4>
           <img src={sortIcon} alt="sort-icon-up-and-down-arrow" />
         </div>
 
-        <div className="inventories__table-field">
-          <h4 className="inventories__table-header">QTY</h4>
+        <div className="inventory__table-field">
+          <h4 className="inventory__table-header">QTY</h4>
           <img src={sortIcon} alt="sort-icon-up-and-down-arrow" />
         </div>
 
-        <div className="inventories__table-field inventories__table-field--position">
-          <h4 className="inventories__table-header">ACTIONS</h4>
+        <div className="inventory__table-field inventory__table-field--position">
+          <h4 className="inventory__table-header">ACTIONS</h4>
         </div>
       </div>
     </>
